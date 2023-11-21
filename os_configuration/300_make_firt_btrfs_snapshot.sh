f582481c1b78021e5f0ca615ff13ea8f535c9591
Michal Schorm <mschorm@redhat.com>
2023-11-21 18:14:11 +0100

    btrfs

--- os_configuration/300_make_firt_btrfs_snapshot.sh
+++ os_configuration/300_make_firt_btrfs_snapshot.sh
@@ -6,19 +6,10 @@
 #   Note: I'm not sure if this command works even before the user logs into the GUI for the first time. Need testing.
 
 
-#device_of_the_root_fs="/dev/sda3"
-#device_of_the_root_fs="/dev/nvmen0p4"
-device_of_the_root_fs=$(mount | grep " / " | awk '{print $1}')
-last_num_cut=${device_of_the_root_fs%?}
-echo ${last_num_cut: -1}
-if [ "${last_num_cut: -1}" = "p" ]; then
-    last_num_cut=${last_num_cut%?}
-fi
-root_fs_name = "$last_num_cut"
-
+btrfs_device=$(mount | grep " / " | awk '{print $1}')
 
 # Mount root subvolume
-mount -t btrfs -o subvol=/ "$root_fs_name" /mnt/ && cd /mnt/ && ls -alh
+mount -t btrfs -o subvol=/ "$btrfs_device" /mnt/ && cd /mnt/ && ls -alh
 # Create readonly snapshot
 btrfs subvolume snapshot -r boot RO-BACKUP_boot_system_setup_finished
 #
