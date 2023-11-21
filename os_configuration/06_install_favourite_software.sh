be452655bb952f08d79b9008592252bc1037fed7
Michal Schorm <mschorm@redhat.com>
2023-11-21 19:05:13 +0100

    pip

--- os_configuration/06_install_favourite_software.sh
+++ os_configuration/06_install_favourite_software.sh
@@ -3,7 +3,7 @@
 #----------------------------------------
 # Install favourite software
 
-dnf -y --comment="Install favourite software" install tree tldr curl tar git zip unzip openssl wget nano terminator fwupd ntfs-3g unicode-emoji "google-noto-emoji*" flatpak dnf-plugin-system-upgrade
+dnf -y --comment="Install favourite software" install tree tldr curl tar git zip unzip openssl wget nano terminator fwupd ntfs-3g unicode-emoji pip "google-noto-emoji*" flatpak dnf-plugin-system-upgrade
 
 #----------------------------------------
 
