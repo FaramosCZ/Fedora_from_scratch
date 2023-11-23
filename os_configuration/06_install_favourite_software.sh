349676b22d63f6e765aa427afa55d6a4e0175fdd
Michal Schorm <mschorm@redhat.com>
2023-11-23 04:13:40 +0100

    Install flatpak discord as a part of favourite software pack

--- os_configuration/06_install_favourite_software.sh
+++ os_configuration/06_install_favourite_software.sh
@@ -5,5 +5,10 @@
 
 dnf -y --comment="Install favourite software" install tree tldr curl tar git zip unzip openssl wget nano terminator fwupd ntfs-3g unicode-emoji pip "google-noto-emoji*" flatpak dnf-plugin-system-upgrade
 
+# Install Discord as a flatpak
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+flatpak install -y flathub com.discordapp.Discord
+
 #----------------------------------------
 
