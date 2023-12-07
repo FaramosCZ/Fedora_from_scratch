152d0ff3c0c3539962928bc75fd9ccffbe43a6d9
Michal Schorm <mschorm@redhat.com>
2023-12-07 02:35:56 +0100

    extend favourite software

--- os_configuration/06_install_favourite_software.sh
+++ os_configuration/06_install_favourite_software.sh
@@ -3,7 +3,7 @@
 #----------------------------------------
 # Install favourite software
 
-dnf -y --comment="Install favourite software" install tree tldr curl tar git zip unzip openssl wget nano terminator fwupd ntfs-3g unicode-emoji pip "google-noto-emoji*" flatpak dnf-plugin-system-upgrade
+dnf -y --comment="Install favourite software" install tree tldr curl tar git zip unzip openssl wget nano terminator fwupd ntfs-3g unicode-emoji pip "google-noto-emoji*" flatpak dnf-plugin-system-upgrade puzzles cmatrix cool-retro-term
 
 # Install Discord as a flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
