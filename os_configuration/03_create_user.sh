44fcf3a5b4b1933604412cff2c430a0f78043f85
Michal Schorm <mschorm@redhat.com>
2023-12-07 02:06:40 +0100

    [specific configuration] Set up user 'faramos'

--- os_configuration/03_create_user.sh
+++ os_configuration/03_create_user.sh
@@ -5,9 +5,9 @@
 #   this is recommended to do before logging to the GUI for the first time,
 #   as logging into GUI as root is sometimes glitchy and not expected in the first place
 
-useradd lod
-usermod -c "Hvězdná loď" lod
-echo -e "lod:lod" | chpasswd
+useradd faramos
+usermod -c "Faramos" faramos
+echo -e "faramos:faramos" | chpasswd
 
 #----------------------------------------
 
--- os_configuration/200_restore_cinnamon_settings.sh
+++ os_configuration/200_restore_cinnamon_settings.sh
@@ -5,11 +5,11 @@
 #   This has to be run as the user (all of the users) that are expected to use the GUI.
 #   Note: I'm not sure if this command works even before the user logs into the GUI for the first time. Need testing.
 
-su -c "dconf load /org/cinnamon/ < ./cinnamon_desktop_backup" lod
+su -c "dconf load /org/cinnamon/ < ./cinnamon_desktop_backup" faramos
 pkill -HUP -f "cinnamon --replace"
 
 # Language & Locale for the non-root user
-su -c "localectl set-locale LANG=en_US.UTF-8" lod
-su -c 'localectl set-x11-keymap cz,us " " , grp:alt_shift_toggle' lod
+su -c "localectl set-locale LANG=en_US.UTF-8" faramos
+su -c 'localectl set-x11-keymap cz,us " " , grp:alt_shift_toggle' faramos
 
 #----------------------------------------
