a3988b52918cf5bf44938a2ec7258fcf968ac07a
Michal Schorm <mschorm@redhat.com>
2023-12-22 06:02:04 +0100

    fwupd doesn't work on Fedora on UEFI (at least for me, it doesn't work at all on UEFI)

--- os_configuration/05_install_GUI.sh
+++ os_configuration/05_install_GUI.sh
@@ -6,7 +6,7 @@
 #   That's really handy, but it also contains a lot of bloatware. Let's keep the package set minimal
 
 dnf -y --comment="Install the Cinnamon DE" groupinstall cinnamon
-dnf -y --comment="Remove the bloatware" -x cinnamon -x xorg-x11-* -x lsof -x boost* -x chrony -x "flatpak*" remove "dnfdragora-*" pidgin xfburn thunderbird xawtv shotwell ImageMagick "*anaconda*" trousers yelp redshift mpv gnome-calculator gnome-calendar plymouth
+dnf -y --comment="Remove the bloatware" -x cinnamon -x xorg-x11-* -x lsof -x boost* -x chrony -x "flatpak*" remove "dnfdragora-*" pidgin xfburn thunderbird xawtv shotwell ImageMagick "*anaconda*" trousers yelp redshift mpv gnome-calculator gnome-calendar plymouth "fwupd*"
 
 dnf -y --comment="Install the Audio Tooling" install alsa-utils pulseaudio-utils
 dnf -y --comment="Install the Audio Firmware" install alsa-sof-firmware
