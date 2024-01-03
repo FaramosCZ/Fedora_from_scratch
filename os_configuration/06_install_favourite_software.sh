bb1e24b7b9ad9b2e50475a039a6cd06b13be018d
Michal Schorm <mschorm@redhat.com>
2024-01-03 13:29:51 +0100

    Split fonts out of favourite sofwtare and add Dejavu fonts needed for Starship prompt character

--- os_configuration/06_install_favourite_software.sh
+++ os_configuration/06_install_favourite_software.sh
@@ -3,7 +3,8 @@
 #----------------------------------------
 # Install favourite software
 
-dnf -y --comment="Install favourite software" install tree tldr curl tar git zip unzip openssl wget nano terminator ntfs-3g unicode-emoji pip "google-noto-emoji*" flatpak dnf-plugin-system-upgrade puzzles cmatrix cool-retro-term
+dnf -y --comment="Install favourite software" install tree tldr curl tar git zip unzip openssl wget nano terminator ntfs-3g pip flatpak dnf-plugin-system-upgrade puzzles cmatrix cool-retro-term
+dnf -y --comment="Install fonts" install unicode-emoji "google-noto-emoji*" dejavu-fonts-all
 
 # Install Discord as a flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
