ef06bd9e4685e5491cd2dbbc1c9591d1a3132bfa
Michal Schorm <mschorm@redhat.com>
2024-01-04 13:47:20 +0100

    [bugfix] IP in TUI

--- os_configuration/00_display_IP_in_TUI.sh
+++ os_configuration/00_display_IP_in_TUI.sh
@@ -3,8 +3,10 @@
 #----------------------------------------
 # Display the current LAN IP address before the login prompts on TUI TTYs
 
+LAN=$(ip -br a | grep enp | awk '{print $1}')
+
 echo -e "\
-IP LAN: \4{enp2s0}
+IP LAN: \4{"$LAN"}
 " > /etc/issue
 
 #----------------------------------------
