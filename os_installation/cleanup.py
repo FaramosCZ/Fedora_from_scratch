44ab30c006e022dc32cff7d9b9da4ffc9322406b
Michal Schorm <mschorm@redhat.com>
2024-01-05 20:05:04 +0100

    Add support for disk encryption by LUKS

--- os_installation/cleanup.py
+++ os_installation/cleanup.py
@@ -10,6 +10,9 @@ shell_cmd('swapoff -a', ignore_error_code=True)
 shell_cmd(f'swapoff {disk_path}*', ignore_error_code=True)
 shell_cmd(f'umount -l {disk_path}*', ignore_error_code=True)
 shell_cmd(f'umount -R -c {mountpoint_path}/*', ignore_error_code=True)
+shell_cmd(f'umount -l /dev/mapper/decrypted_boot', ignore_error_code=True)
+shell_cmd(f'umount -R -c /dev/mapper/decrypted_boot', ignore_error_code=True)
+shell_cmd(f'cryptsetup close decrypted_boot', ignore_error_code=True)
 shell_cmd('sync ; sleep 3', False, False, True)
 
 #----------------------------------------
