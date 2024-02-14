de3efc6f55757ff60122e1fb77cb04d9c620afba
Michal Schorm <mschorm@redhat.com>
2024-02-15 00:55:54 +0100

    [workaround] Fix annoying bluetooth rfkill pop-up after every login

--- os_configuration/03_create_user.sh
+++ os_configuration/03_create_user.sh
@@ -11,5 +11,10 @@ useradd "$USER"
 usermod -c "$USER_PRETTY" "$USER"
 echo -e "$USER:$USER" | chpasswd
 
+# Bluetooth GUI client 'bluez' utilizes polkit rules, which require the user to be in the 'wheel' group,
+# in order to be able to control the BT ON/OFF state
+# Otherwise it will spam the user with very annoying pop-up window asking for root password after every login.
+usermod -a -G wheel "$USER"
+
 #----------------------------------------
 
