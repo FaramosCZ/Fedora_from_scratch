70036189efe1673f40e7630a78226fc56531b5e8
Michal Schorm <mschorm@redhat.com>
2024-08-02 20:02:21 +0200

    [specific configuration] Add software requested by the user

--- os_configuration/06_install_favourite_software.sh
+++ os_configuration/06_install_favourite_software.sh
@@ -6,11 +6,16 @@
 dnf -y --comment="Install favourite software" install tree tldr curl tar git zip unzip openssl wget nano terminator ntfs-3g pip flatpak dnf-plugin-system-upgrade puzzles cmatrix cool-retro-term
 dnf -y --comment="Install fonts" install unicode-emoji "google-noto-emoji*" dejavu-fonts-all
 
+dnf -y --comment="Install user requested software" install chromium libreoffice inkscape
+
 # Install Discord as a flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 flatpak install -y flathub com.discordapp.Discord
 flatpak install -y flathub io.freetubeapp.FreeTube
+
+flatpak install -y flathub us.zoom.Zoom
+flatpak install -y flathub com.skype.Client
 
 #----------------------------------------
 
