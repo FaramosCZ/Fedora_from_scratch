d7e9b7ed8e198216193f7fa9b2e662a38cb8c2d6
Michal Schorm <mschorm@redhat.com>
2024-08-11 02:08:47 +0200

    additional software ruqested

--- os_configuration/06_install_favourite_software.sh
+++ os_configuration/06_install_favourite_software.sh
@@ -6,7 +6,7 @@
 dnf -y --comment="Install favourite software" install tree tldr curl tar git zip unzip openssl wget nano terminator ntfs-3g pip flatpak dnf-plugin-system-upgrade puzzles cmatrix cool-retro-term
 dnf -y --comment="Install fonts" install unicode-emoji "google-noto-emoji*" dejavu-fonts-all
 
-dnf -y --comment="Install user requested software" install chromium libreoffice inkscape
+dnf -y --comment="Install user requested software" install chromium libreoffice inkscape gnome-calculator
 
 # Install Discord as a flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
