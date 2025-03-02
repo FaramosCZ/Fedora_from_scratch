decbf78c0925e90fbc7d9420ded958ea60872a68
Michal Schorm <mschorm@redhat.com>
2025-03-02 22:36:28 +0100

    [enhancement] Employ more capabilities of 'read'

--- os_configuration/00_add_hostname.sh
+++ os_configuration/00_add_hostname.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
 #----------------------------------------
-# Display the current LAN IP address before the login prompts on TUI TTYs
 
-#!/bin/bash
-
-# Prompt the user for input
-echo "Set this machine HOSTNAME: "
-read new_hostname
+read -p "Set this machine HOSTNAME: " -e -i "FARAMOS-NTB-" new_hostname
 
 echo "$new_hostname" > /etc/hostname
 
