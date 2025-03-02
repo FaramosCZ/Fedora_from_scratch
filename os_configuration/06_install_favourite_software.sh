05a14417681633e3a1faabc52774d3150802fdd2
Michal Schorm <mschorm@redhat.com>
2025-03-02 22:24:21 +0100

    [enhancement] Install color emoji fonts instead of black and white only variant

--- os_configuration/06_install_favourite_software.sh
+++ os_configuration/06_install_favourite_software.sh
@@ -4,7 +4,7 @@
 # Install favourite software
 
 dnf -y --comment="Install favourite software" install tree tldr curl tar git zip unzip unrar openssl wget nano terminator ntfs-3g pip flatpak dnf-plugin-system-upgrade puzzles cmatrix cool-retro-term rsync
-dnf -y --comment="Install fonts" install unicode-emoji "google-noto-emoji*" dejavu-fonts-all
+dnf -y --comment="Install fonts" install "google-noto-color-emoji*" dejavu-fonts-all
 
 # Install Discord as a flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
