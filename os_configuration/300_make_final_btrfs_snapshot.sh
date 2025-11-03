f2d4d62cda5a2ae3ea49340398ee67d85e3f9b45
Michal Schorm <mschorm@redhat.com>
2025-11-03 19:20:56 +0100

    [enhancement] Rework the BTRFS snapshot creation

---
+++ os_configuration/300_make_final_btrfs_snapshot.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#----------------------------------------
+# Create a backup - BTRFS snapshot
+
+# Mount BTRFS root subvolume
+#   Use prepared mount configuration from /etc/fstab
+mount --target=/mnt/BTRFS-ROOT && pushd /mnt/BTRFS-ROOT
+
+# Create readonly snapshot
+btrfs subvolume snapshot -r boot RO-BACKUP-02-GUI_setup_finished
+
+popd
+
+#----------------------------------------
