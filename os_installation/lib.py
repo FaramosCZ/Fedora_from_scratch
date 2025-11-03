a237f21545ad6dbf23ae21f3975b9c6b1b847718
Michal Schorm <mschorm@redhat.com>
2025-11-03 18:09:40 +0100

    [enhancement] Color the os_installation output

--- os_installation/lib.py
+++ os_installation/lib.py
@@ -9,9 +9,9 @@ from os import geteuid
 
 # Check if the script is run with root privileges
 if geteuid() == 0:
-    print("RUNNING AS ROOT")
+    print("\033[1;32mRUNNING AS ROOT\033[0m")
 else:
-    print("ERROR: THIS SCRIPT HAS TO BE EXECUTED AS ROOT !")
+    print("\033[1;31mERROR: THIS SCRIPT HAS TO BE EXECUTED AS ROOT !\033[0m")
     exit(1)
 
 # =================================================================================================================
@@ -19,7 +19,7 @@ else:
 def shell_cmd(command, print_stdout=True, print_command=True, ignore_error_code=False):
 
     if print_command:
-        print(f"\nCMD:\n{command}\n")
+        print(f"\033[1m\nCMD:\n{command}\n\033[0m")
 
     # Run the command with shell=True and print output directly
     process = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
@@ -30,7 +30,7 @@ def shell_cmd(command, print_stdout=True, print_command=True, ignore_error_code=
     # Check the return code
     if not ignore_error_code:
         if process.returncode != 0:
-            print("COMMAND FAILED with return code:", process.returncode)
+            print("\033[1;31mCOMMAND FAILED with return code:\033[0m", process.returncode)
             exit(process.returncode)
 
 # =================================================================================================================
