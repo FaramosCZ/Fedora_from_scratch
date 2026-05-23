397c08ca4e443173ee7e3b7f4a4a8301fc760183
Michal Schorm <mschorm@redhat.com>
2026-05-23 17:50:29 +0200

    [specific configuration] Install thunderbolt software

--- os_configuration/032_install_favourite_software.sh
+++ os_configuration/032_install_favourite_software.sh
@@ -5,7 +5,7 @@
 
 dnf -y --comment="CLI tools" install tree tldr curl tar git zip unzip unrar openssl wget nano rsync
 dnf -y --comment="Terminal emulator" install terminator
-dnf -y --comment="System utilities" install ntfs-3g pip flatpak dnf-plugin-system-upgrade gnome-software speedtest-cli
+dnf -y --comment="System utilities" install ntfs-3g pip flatpak dnf-plugin-system-upgrade gnome-software speedtest-cli bolt
 dnf -y --comment="Fonts" install "google-noto-color-emoji*" dejavu-fonts-all
 dnf -y --comment="Web browser" install brave-browser
 dnf -y --comment="Games" install puzzles EmptyEpsilon cmatrix cool-retro-term
