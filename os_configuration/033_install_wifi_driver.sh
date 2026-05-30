519b31be704a465d02d87b67a8aab804cb2fc87a
Michal Schorm <mschorm@redhat.com>
2026-05-30 20:41:28 +0200

    Install missing wi-fi driver

--- os_configuration/033_install_wifi_driver.sh
+++ os_configuration/033_install_wifi_driver.sh
@@ -4,7 +4,7 @@
 # Install the general wi-fi drivers
 #   The system has to be rebooted in order to this to take effect
 
-dnf -y --comment="Install the general wi-fi drivers" install iwlwifi-mvm-firmware iwlwifi-dvm-firmware
+dnf -y --comment="Install the general wi-fi drivers" install iwlwifi-mvm-firmware iwlwifi-dvm-firmware iwlegacy-firmware
 
 # In case of issue package 'iwlegacy-firmware' can be tried
 # or package 'broadcom-wl', but it taints the kernel
