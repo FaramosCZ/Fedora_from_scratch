b12aef5e1a793b63730d19c1b201cee8b14e3832
Michal Schorm <mschorm@redhat.com>
2026-06-01 15:57:12 +0200

    [specific configuration] Set disk to 'mmcblk1'

--- os_installation/config.py
+++ os_installation/config.py
@@ -3,6 +3,6 @@ fedora_release=44
 # Needed for cleanup.py
 mountpoint_path = "/mnt/FEDORA_FROM_SCRATCH"
 
-disk = "sda"
+disk = "mmcblk1"
 device_name="FEDORA-FROM-SCRATCH"
 disk_path = f"/dev/{disk}"
